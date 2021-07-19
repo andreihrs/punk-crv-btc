54851064c16d02a42ecfd41486e6c28470d92bb5
andreihrs <andrei_hristescu@yahoo.com>
2021-07-19 17:25:09 +0300

    fix deployment addresses

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -16,17 +16,28 @@ async function main(): Promise<void> {
   let forge: SignerWithAddress;
 
   [forge] = await ethers.getSigners();
-  const WBTC = "0x3b92f58feD223E2cB1bCe4c286BD97e42f2A12EA";
-  const UniswapV2Router = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-  const btcCrv = "0xa1faa15655b0e7b6b6470ed3d096390e6ad93abb";
-  const crv = "0x61460874a7196d6a22d1ee4922473664b3e95270";
+
+  const WBTC_ADDRESS = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
+  const UNISWAPV2_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+  const CRV_BTC_ADDRESS = "0x49849C98ae39Fff122806C06791Fa73784FB3675";
+  const CRV_ADDRESS = "0xD533a949740bb3306d119CC777fa900bA034cd52";
+  const CRV_POOL_ADDRESS = "0x93054188d876f558f4a66B2EF1d97d16eDf0895B";
+  const CRV_REWARD_POOL_ADDRESS = "0xB1F2cdeC61db658F091671F5f199635aEF202CAC";
 
   const CrbWbtcModel: ContractFactory = await ethers.getContractFactory("CrvWbtcModel");
-  const wbtcModel: Contract = await WbtcModel.deploy();
+  const wbtcModel: Contract = await CrbWbtcModel.deploy();
   await wbtcModel.deployed();
   console.log("Model deployed to: ", wbtcModel.address);
 
-  await wbtcModel.initialize(forge.address, WBTC, cWBTC, COMP, comptroller, UniswapV2Router);
+  await wbtcModel.initialize(
+    forge.address,
+    WBTC_ADDRESS,
+    CRV_BTC_ADDRESS,
+    CRV_ADDRESS,
+    CRV_POOL_ADDRESS,
+    CRV_REWARD_POOL_ADDRESS,
+    UNISWAPV2_ADDRESS,
+  );
 
   // console.log(await wbtcModel.invest());
 }
